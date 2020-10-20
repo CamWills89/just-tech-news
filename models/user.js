7cce0a65f575550539f9873a642c0d51abb7751b
Cameron <cameron.wills89@outlook.com>
2020-10-20 09:57:04 -0400

    added in password hashing for creating and updating users

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+//importing password hashing to protect users passwords from attacks
+const bcrypt = require("bcrypt");
 
 // create our User model extended off of the Sequelize Model class.
 //this allows User to inherit functionality from Model
@@ -8,56 +10,82 @@ class User extends Model {}
 
 // define table columns and configuration
 User.init(
-    {
-      // define an id column
-      id: {
-        // use the special Sequelize DataTypes object to provide what type of data it is
-        type: DataTypes.INTEGER,
-        // this is the equivalent of SQL's `NOT NULL` option
-        allowNull: false,
-        // instruct that this is the Primary Key
-        primaryKey: true,
-        // turn on auto increment
-        autoIncrement: true
-      },
-      // define a username column
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      // define an email column
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        // there cannot be any duplicate email values in this table
-        unique: true,
-        // if allowNull is set to false, we can run our data through validators before creating the table data
-        validate: {
-          isEmail: true
-        }
-      },
-      // define a password column
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          // this means the password must be at least four characters long
-          len: [4]
-        }
-      }
+  {
+    // define an id column
+    id: {
+      // use the special Sequelize DataTypes object to provide what type of data it is
+      type: DataTypes.INTEGER,
+      // this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+      // instruct that this is the Primary Key
+      primaryKey: true,
+      // turn on auto increment
+      autoIncrement: true,
     },
-    {
+    // define a username column
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // define an email column
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // there cannot be any duplicate email values in this table
+      unique: true,
+      // if allowNull is set to false, we can run our data through validators before creating the table data
+      validate: {
+        isEmail: true,
+      },
+    },
+    // define a password column
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        // this means the password must be at least four characters long
+        len: [4],
+      },
+    },
+  },
+  {
+    //the nested level of the objectd inserted is NB.
+    //It's been added to the second object of the User.init()
+    //this hook needs to fire before a new user is created, so beforeCreate is appropriate
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      // beforeCreate(userData) {
+      //   return bcrypt.hash(userData.password, 10).then((newUserData) => {
+      //     return newUserData;
+      //   });
+      // },
+      //replacing above with the new async/await syntax for better legibility
+      //the 10 is the saltRounds
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set up beforeUpdate lifecycle "hook" functionality if they want to change their password
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
     // pass in our imported sequelize connection (the direct connection to our database)
-      sequelize,
+    sequelize,
     // don't automatically create createdAt/updatedAt timestamp fields
-      timestamps: false,
+    timestamps: false,
     // don't pluralize name of database table
-      freezeTableName: true,
+    freezeTableName: true,
     // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-      underscored: true,
+    underscored: true,
     // make it so our model name stays lowercase in the database
-      modelName: 'user'
-    }
-  );
+    modelName: "user",
+  }
+);
 
 module.exports = User;
